a649dc90933f00619badc24d8d91f22430d440f0
Artyom Palevich <hello@apalevich.ru>
2022-01-24 20:23:01 +0700

    Created API client as a class

---
+++ src/index.js
@@ -0,0 +1,51 @@
+class SwapiService {
+
+  _baseUrl = 'https://swapi.dev/api';
+
+  async getResource(path) {
+    const res = await fetch(`${this._baseUrl}${path}`);
+
+    if (!res.ok) {
+      throw new Error(`Could not fetch ${path}, received ${res.status}`)
+    }
+    return await res.json();
+    
+  }
+
+  async getAllPeople() {
+    const res = await this.getResource('/people/');
+    return res.results;
+  }
+
+  getPerson(id) {
+    return this.getResource(`/people/${id}/`)
+  }
+
+  async getAllPlanets() {
+    const res = await this.getResource('/planets/');
+    return res.results;
+  }
+
+  getPlanet(id) {
+    return this.getResource(`/planets/${id}/`)
+  }
+
+  async getAllStarships() {
+    const res = await this.getResource('/starships/');
+    return res.results;
+  }
+
+  getStarship(id) {
+    return this.getResource(`/starships/${id}/`)
+  }
+
+}
+
+const swapi = new SwapiService();
+
+swapi.getAllPeople().then(console.log)
+swapi.getPerson(3).then(console.log)
+swapi.getAllPlanets().then(console.log)
+swapi.getPlanet(3).then(console.log)
+swapi.getAllStarships().then(console.log)
+swapi.getStarship(3).then(console.log)
